bb89a7f006bd5716a97bf9a570599776e0d19d6e
Xavier <cestmoibalibalo@users.noreply.github.com>
2019-05-16 20:18:38 +0200

    Continuous optimization

--- MyPythonProject/Applications/Images/shared.py
+++ MyPythonProject/Applications/Images/shared.py
@@ -2,20 +2,23 @@
 # pylint: disable=invalid-name
 import json
 import os
-import sqlite3
 from collections import OrderedDict
 from contextlib import suppress
 from pathlib import PurePath
 from subprocess import PIPE, run
 from tempfile import TemporaryFile
-from typing import Tuple, Union
+from typing import Dict, Iterable, Optional, Tuple, Union
+
+import yaml
+
+from ..Tables.shared import DatabaseConnection
 
 __author__ = 'Xavier ROSSET'
 __maintainer__ = 'Xavier ROSSET'
 __email__ = 'xavier.python.computing@protonmail.com'
 __status__ = "Development"
 
-that_file = os.path.abspath(__file__)
+that_file = PurePath(os.path.abspath(__file__))
 
 # ==================
 # Functions aliases.
@@ -27,25 +30,34 @@ expandvars = os.path.expandvars
 # Public interface.
 # =================
 def main(database: Union[str, PurePath], *paths: Union[str, PurePath], delete: bool = True) -> Tuple[int, int]:
-    conn = sqlite3.connect(str(database))
-    deleted = 0
-    if delete:
-        deleted = _delete_record(conn, *paths)
-    collection = [("/".join(PurePath(file).parts[1:-1]), str(PurePath(file).name), datetimeoriginal) for file, datetimeoriginal in _get_metadata(*paths)]
-    inserted = _insert_record(conn, *collection) - deleted
-    conn.close()
+    deleted, inserted = 0, 0
+    with DatabaseConnection(str(database)) as conn:
+        if delete:
+            deleted = _delete_records(conn, *paths)
+        collection = [("/".join(PurePath(file).parts[1:-1]), str(PurePath(file).name), datetimeoriginal, year, month, day, strategy) for file, (datetimeoriginal, year, month, day, strategy) in
+                      _get_metadata(*paths)]
+        inserted = _insert_records(conn, *collection) - deleted
     return deleted, inserted
+
+
+def select(database: Union[str, PurePath], year: Optional[int] = None) -> Iterable[Tuple[str, str, int, int, int, int, int]]:
+    with DatabaseConnection(str(database)) as conn:
+        for item in _select_records(conn, year=year):
+            yield item
 
 
 # ===================
 # Private interfaces.
 # ===================
 def _get_metadata(*paths: Union[str, PurePath]):
-    collection = OrderedDict()
+    strategies = {}
+    with open(that_file.parent / "Resources" / "strategies.yml") as stream:
+        strategies = yaml.load(stream)
+    collection = OrderedDict()  # type: Dict[str, Tuple[int, int, int, int, int]]
     args = [str(PurePath(expandvars("%_RESOURCES%")) / "exiftool.exe"),
             "-r",
             "-d",
-            "%Y%m%d%H%M%S",
+            "%Y%m%d",
             "-j",
             "-charset",
             "Latin1",
@@ -54,30 +66,49 @@ def _get_metadata(*paths: Union[str, PurePath]):
             "DateTimeOriginal",
             "-ext",
             "jpg"]
-    with TemporaryFile(mode="r+", encoding="UTF_8") as stream:
-        args.extend(sorted(map(str, paths)))
-        process = run(args, stdout=stream, stderr=PIPE, universal_newlines=True)
-        if not process.returncode:
-            stream.seek(0)
-            for item in json.load(stream):
-                with suppress(KeyError):
-                    key = item["SourceFile"]
-                    value = int(str(item["DateTimeOriginal"])[:8])
-                    collection[key] = value
+    for path in paths:
+        with TemporaryFile(mode="r+", encoding="UTF_8") as stream:
+            _args = list(args)
+            _args.append(str(path))
+            _year = int(PurePath(path).parts[1][:4])  # type: int
+            process = run(_args, stdout=stream, stderr=PIPE, universal_newlines=True)
+            if not process.returncode:
+                stream.seek(0)
+                try:
+                    mapping = json.load(stream)
+                except json.decoder.JSONDecodeError:
+                    pass
+                else:
+                    for item in mapping:
+                        with suppress(KeyError):
+                            key = item["SourceFile"]  # type: str
+                            value = item["DateTimeOriginal"]  # type: int
+                            collection[key] = (value, int(str(value)[:4]), int(str(value)[4:6]), int(str(value)[6:8]), strategies.get(_year, 0))
     for item in collection.items():
         yield item
 
 
-def _delete_record(db_connector, *args: Union[str, PurePath]) -> int:
+def _delete_records(db_connector, *args: Union[str, PurePath]) -> int:
     with db_connector:
         db_connector.executemany("DELETE FROM images WHERE parent=?", [("/".join(path.parts[1:]),) for path in map(PurePath, args)])
     return db_connector.total_changes
 
 
-def _insert_record(db_connector, *args: Tuple[str, str, int]) -> int:
+def _insert_records(db_connector, *args: Tuple[str, str, int, int, int, int, int]) -> int:
     with db_connector:
-        db_connector.executemany("INSERT INTO images (parent, name, datetimeoriginal) VALUES(?, ?, ?)", args)
+        db_connector.executemany("INSERT INTO images (parent, name, datetimeoriginal, year, month, day, strategy) VALUES(?, ?, ?, ?, ?, ?, ?)", args)
     return db_connector.total_changes
+
+
+def _select_records(db_connector, year=None):
+    statement = "SELECT parent, name, datetimeoriginal, year, month, day, strategy FROM images"
+    args = ()
+    if year:
+        statement = f"{statement} WHERE year=?"
+        args = (year,)
+    statement = f"{statement} ORDER BY year, month, name"
+    for row in db_connector.execute(statement, args):
+        yield (row["parent"], row["name"], row["datetimeoriginal"], row["year"], row["month"], row["day"], row["strategy"])
 
 
 # =============================
@@ -86,12 +117,35 @@ def _insert_record(db_connector, *args: Tuple[str, str, int]) -> int:
 if __name__ == "__main__":
     import argparse
     import operator
+    import re
     from Applications.parsers import database_parser
 
+
+    class SetPurePath(argparse.Action):
+        """
+
+        """
+
+        def __init__(self, option_strings, dest, **kwargs):
+            super().__init__(option_strings, dest, **kwargs)
+
+        def __call__(self, parsobj, namespace, values, option_string=None):
+            setattr(namespace, self.dest, list(map(PurePath, values)))
+
+
+    regex = re.compile(r"^\d{4,6}$")
+
     parser = argparse.ArgumentParser(parents=[database_parser])
-    parser.add_argument("path", nargs="+")
+    parser.add_argument("paths", nargs="+", action=SetPurePath)
     parser.add_argument("--dont_delete", action="store_true")
     arguments = parser.parse_args()
-    _deleted, _inserted = main(arguments.db, *arguments.path, delete=operator.not_(arguments.dont_delete))
-    print(_deleted)
-    print(_inserted)
+    for path in arguments.paths:
+        for root, dirs, files in os.walk(path):
+            if files:
+                match = regex.match(PurePath(root).parts[1])
+                if match:
+                    deleted, inserted = main(arguments.db, PurePath(root), delete=operator.not_(arguments.dont_delete))
+                    print("# ----- #")
+                    print(root)
+                    print(deleted)
+                    print(inserted)
